8be089f14ce56fa8469cbbcf5f60bf48673521b3
adhammhaydat <adhammohidat123@gmail.com>
2021-06-07 06:28:26 -0700

    new style

---
+++ javascript/app.js
@@ -0,0 +1,30 @@
+'use strict';
+let userName = prompt('what is your name');
+alert('welcome  '+userName); 
+
+let juice= prompt('Do you think I like juice').toLowerCase();
+if(juice ==='yes'){
+    console.log('your answer is true');
+    alert('good job '+userName);
+}
+
+let tea= prompt('Do you think I like tea').toLowerCase();
+if(tea ==='yes'){
+    console.log('your answer is true');
+    alert('great '+userName);
+}
+let coffee= prompt('Do you think I like coffee').toLowerCase();
+if(coffee ==='yes'){
+    console.log('your answer is true');
+    alert('good answer '+userName);
+}
+let cats= prompt('Do you think I like cats').toLowerCase();
+if(cats ==='yes'){
+    console.log('your answer is true');
+    alert('wow nice '+userName);
+}
+let movies= prompt('Do you think I like movies').toLowerCase();
+if(movies ==='yes'){
+    console.log('your answer is true');
+    alert('yes  '+userName);
+}
