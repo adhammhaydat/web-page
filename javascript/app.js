5198c353d892b578b51bf93734b10e5d118fb425
adhammhaydat <adhammohidat123@gmail.com>
2021-06-08 05:14:31 -0700

    new codes

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,49 @@
 'use strict';
 let userName = prompt('what is your name?');
-alert('welcome  '+userName); 
+alert('welcome  '+userName);
 
 let count=0;
+
+let juice= prompt('Do you think I like juice').toLowerCase();
+if(juice ==='yes'){
+    console.log('your answer is true');
+    alert('good job '+userName);
+    count++;
+
+}
+
+
+let tea= prompt('Do you think I like tea').toLowerCase();
+if(tea ==='yes'){
+    console.log('your answer is true');
+    alert('great '+userName);
+    count++;
+
+}
+
+let coffee= prompt('Do you think I like coffee').toLowerCase();
+if(coffee ==='yes'){
+    console.log('your answer is true');
+    alert('good answer '+userName);
+    count++;
+
+}
+let cats= prompt('Do you think I like cats').toLowerCase();
+if(cats ==='yes'){
+    console.log('your answer is true');
+    alert('wow nice '+userName);
+    count++;
+
+}
+let movies= prompt('Do you think I like movies').toLowerCase();
+if(movies ==='yes'){
+        console.log('your answer is true');
+        alert('yes  '+userName);
+        count++;
+
+}
+
+
 let age = prompt('what is my age?');
 age =parseInt(age)
 if(age == 23){
@@ -10,67 +51,6 @@ if(age == 23){
     alert('Right. Well done');
     count++;
 }else if(age >=20 && age <=25){
-    alert('very close')
-}else{
-    alert('so far')
-}
-
-let weight= prompt('what is my weight?');
-weight = parseInt(weight);
-if(weight == 80){
-    console.log('Right. Well done');
-    alert('Right. Well done');
-    count++;
-}else if(weight >=70 && weight <=85){
-    alert('very close')
-}else{
-    alert('so far')
-}
-
-let height= prompt('what is my height?');
-height = parseInt(height)
-if(height == 176){
-    console.log('Right. Well done');
-    alert('Right. Well done');
-    count++;
-}else if(height >=170 && height <=180){
-    alert('very close')
-}else{
-    alert('so far')
-}
-
-let speed= prompt('What is the maximum speed I can reach in the car?');
-speed = parseInt(speed);
-if(speed == 180){
-    console.log('Right. Well done');
-    alert('Right. Well done');
-    count++;
-}else if(speed >=170 && speed <=190){
-    alert('very close')
-}else{
-    alert('so far')
- }
-
-let mark= prompt('What is the last mark I got?');
-mark = parseInt(mark)
-if(mark == 100){
-    console.log('Right. Well done');
-    alert('Right. Well done');
-    count++;
-}else if(mark >=90 && mark <100){
-    alert('very close')
-}else{
-    alert('so far')
-}
-
-
-let minute= prompt('How long did it take to build this website(in minute)?');
-minute = parseInt(minute);
-if(minute == 40){
-    console.log('Right. Well done');
-    alert('Right. Well done');
-    count++;
-}else if(minute >=25 && minute <50){
     alert('very close')
 }else{
     alert('so far')
